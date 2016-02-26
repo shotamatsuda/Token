66c7d428948c1a590fff1f179e6aaf4f84c3e276
Shota Matsuda <shota@takram.com>
2016-02-26 14:02:05 +0900

    converting objective-c source to swift

---
+++ objc/Typeface.swift
@@ -0,0 +1,9 @@
+//
+//  Typeface.swift
+//  Token
+//
+//  Created by Shota Matsuda on 16/2/24.
+//  Copyright © 2016 takram design engineering. All rights reserved.
+//
+
+import Foundation
---
+++ objc/TypefaceViewController.swift
@@ -0,0 +1,9 @@
+//
+//  TypefaceViewController.swift
+//  Token
+//
+//  Created by Shota Matsuda on 16/2/24.
+//  Copyright © 2016 takram design engineering. All rights reserved.
+//
+
+import Foundation
