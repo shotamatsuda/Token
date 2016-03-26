dbdb50ff00e1be60ae8feaf8c77858193d3f1169
Shota Matsuda <shota@takram.com>
2016-03-27 01:56:57 +0900

    added tx

---
+++ src/token/afdko/ttx.h
@@ -0,0 +1,43 @@
+//
+//  token/afdko/makeotf.h
+//
+//  The MIT License
+//
+//  Copyright (C) 2015-2016 Shota Matsuda
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a
+//  copy of this software and associated documentation files (the "Software"),
+//  to deal in the Software without restriction, including without limitation
+//  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//  and/or sell copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+//  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//  DEALINGS IN THE SOFTWARE.
+//
+
+#pragma once
+#ifndef TOKEN_AFDKO_TTX_H_
+#define TOKEN_AFDKO_TTX_H_
+
+#include <string>
+
+#include "token/ufo.h"
+
+namespace token {
+namespace afdko {
+
+bool ttx(const std::string& tools, const std::string& input);
+
+}  // namespace afdko
+}  // namespace token
+
+#endif  // TOKEN_AFDKO_TTX_H_
